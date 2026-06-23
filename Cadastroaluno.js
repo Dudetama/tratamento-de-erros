12cd4cf6c8065f90d7881425fc6badc6eda2adf2
sx <taylor.vieira@estudante.ifms.edu.br>
2026-06-22 23:12:20 -0300

    final

--- Cadastroaluno.js
+++ Cadastroaluno.js
@@ -17,4 +17,38 @@ class CadastroAluno {
             throw new Error("E-mail inválido.");
         }
 
-      
+        const regexSenha = /^(?=.*\d).{8,}$/;
+        if (!regexSenha.test(aluno.senha)) {
+            throw new Error("Senha inválida. Deve ter ao menos 8 caracteres e 1 número.");
+        }
+    }
+
+    salvar(aluno) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("Aluno cadastrado com sucesso!");
+            }, 3000);
+        });
+    }
+}
+const aluno = new Aluno("João", "joao@gmail.com", "abc12345");
+const cadastro = new CadastroAluno();
+
+async function executar() {
+    console.log("Iniciando processo..."); 
+    try {
+        cadastro.validar(aluno);
+        
+        console.log("Dados válidos! Salvando no banco...");
+        const resposta = await cadastro.salvar(aluno);
+        console.log(resposta); 
+        
+    } catch (erro) {
+        console.log("Erro no cadastro: " + erro.message);
+        
+    } finally {
+        console.log("Processo de cadastro finalizado.");
+    }
+}
+
+executar();
